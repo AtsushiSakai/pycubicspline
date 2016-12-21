34859acd83fb305f6536e0fe8e9f0d4ce387d101
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-12-20 22:58:51 -0800

    add test code

---
+++ tests/test_pycubicspline.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+# --*-- coding:utf-8 --*--
+u"""
+Test
+"""
+
+import unittest
+import pycubicspline
+
+
+class Test(unittest.TestCase):
+
+    def test_1(self):
+        x = [-0.5, 0.0, 0.5, 1.0, 1.5]
+        y = [3.2, 2.7, 6, 5, 6.5]
+        sp = pycubicspline.Spline(x, y)
+        self.assertEqual(sp.a[0], 3.2)
+        self.assertEqual(sp.a[1], 2.7)
+        self.assertEqual(sp.a[2], 6.0)
+        self.assertEqual(sp.a[3], 5.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
