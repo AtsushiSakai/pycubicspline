65228172ad9479c52e45fcb76c697997c9927917
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-12-22 22:23:13 -0800

    add test

--- tests/test_pycubicspline.py
+++ tests/test_pycubicspline.py
@@ -10,7 +10,7 @@ import pycubicspline
 
 class Test(unittest.TestCase):
 
-    def test_1(self):
+    def test_param(self):
         x = [-0.5, 0.0, 0.5, 1.0, 1.5]
         y = [3.2, 2.7, 6, 5, 6.5]
         sp = pycubicspline.Spline(x, y)
@@ -18,6 +18,10 @@ class Test(unittest.TestCase):
         self.assertEqual(sp.a[1], 2.7)
         self.assertEqual(sp.a[2], 6.0)
         self.assertEqual(sp.a[3], 5.0)
+
+        self.assertEqual(sp.b[0], -3.7392857142857139)
+        self.assertEqual(sp.b[1], 4.4785714285714278)
+        self.assertEqual(sp.b[2], 2.6249999999999991)
 
 
 if __name__ == '__main__':
